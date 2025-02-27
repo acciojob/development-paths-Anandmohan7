ccdf2ddf2d5ccc129bc0e6cb26acd42f6539049c
Anand Mohan <99183883+Anandmohan7@users.noreply.github.com>
2025-02-27 20:37:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-//your JS code here. If required.
-  document.addEventListener("DOMContentLoaded", function() {
-            let listItems = document.querySelectorAll("ul li");
-            listItems.forEach(item => {
-                item.textContent += " development";
-            });
-        });
+// //your JS code here. If required.
+//   document.addEventListener("DOMContentLoaded", function() {
+//             let listItems = document.querySelectorAll("ul li");
+//             listItems.forEach(item => {
+//                 item.textContent += " development";
+//          });
+//         });
+
+
+document.addEventListener("DOMContentLoaded", () => 
+    document.querySelectorAll("ul li").forEach(item => item.append(" development"))
+);
+H
