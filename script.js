25086ad22c3a669a5a15c762b48e42f32be1e48c
Anand Mohan <99183883+Anandmohan7@users.noreply.github.com>
2025-02-27 19:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+  document.addEventListener("DOMContentLoaded", function() {
+            let listItems = document.querySelectorAll("ul li");
+            listItems.forEach(item => {
+                item.textContent += " development";
+            });
+        });
